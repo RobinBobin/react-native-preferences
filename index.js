6e673b04910530d7409e56a3afcb5ea8d3278a0c
Ruben Shalimov <r_shalimov@inbox.ru>
2021-03-21 15:07:40 +0300

    0.1.0 - First version.

---
+++ index.js
@@ -0,0 +1,16 @@
+
+import ArrayPreference from "./js/ArrayPreference";
+import JSONPreference from "./js/JSONPreference"
+import NumberPreference from "./js/NumberPreference";
+import Preference from "./js/Preference";
+import Preferences from "./js/Preferences";
+import SwitchPreference from "./js/SwitchPreference";
+
+export {
+  ArrayPreference,
+  JSONPreference,
+  NumberPreference,
+  Preference,
+  Preferences,
+  SwitchPreference
+};
